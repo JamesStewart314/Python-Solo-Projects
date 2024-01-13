5a07c1c31271f5f00f893862e6474f72adae87be
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-01-13 01:22:57 -0300

    Add files via upload

---
+++ Prime_Number_Tools.py
@@ -0,0 +1,42 @@
+from typing import Generator
+
+
+def check_primality(number: int, /) -> bool:
+    if not isinstance(number, int) or number <= 0:
+        raise ValueError('To check primality, the number must be an positive integer.')
+    elif number in (1, 4):
+        return False
+    elif number in (2, 3):
+        return True
+
+    for i in range(2, int(number ** (1 / 2)) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+def prime_generator() -> Generator[int, None, None]:
+    counter: int = 1
+
+    while True:
+        if check_primality(counter):
+            yield counter
+        counter += 1
+
+
+def first_n_primes(quantity: int, /) -> list[int]:
+
+    if not isinstance(quantity, int) or quantity < 0:
+        raise ValueError('Quantity must be an positive integer.')
+
+    primes: list[int] = []
+    size: int = 0
+
+    primes_generator: Generator[int, None, None] = prime_generator()
+
+    while size < quantity:
+        primes.append(next(primes_generator))
+        size += 1
+
+    return primes
+
