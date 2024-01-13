0f5a67f6da829aa3b3ff8d97ee69839510e7b6b7
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-01-12 22:16:06 -0300

    Update Determinant Calculator.py

--- Determinant Calculator.py
+++ Determinant Calculator.py
@@ -1,20 +1,25 @@
-from typing import List
+# Python 3.12+
+
+type OptionalNumber = int | float | None
 
 
-def determinant(matrix: List[List[int | float]]) -> int | float | None:
+def determinant(matrix: list[list[int | float]], /) -> OptionalNumber:
 
     if matrix:  # if the Matrix is not empty
         if len(matrix) > 1:
 
-            order = len(matrix[0])
+            order: int = len(matrix[0])
             for row in matrix:
                 if len(row) != order:
-                    print("This is not an Matrix.")
+                    if __name__ == '__main__':
+                        print("This is not an Matrix.")
                     return None
 
             if len(matrix) == len(matrix[0]):
+
                 if len(matrix) == 2:
                     return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
                 elif len(matrix) == 3:
                     principal_diagonal_1 = matrix[0][0] * matrix[1][1] * matrix[2][2]
                     principal_diagonal_2 = matrix[1][0] * matrix[2][1] * matrix[0][2]
@@ -28,57 +33,72 @@ def determinant(matrix: List[List[int | float]]) -> int | float | None:
                     second_sum = secondary_diagonal_1 + secondary_diagonal_2 + secondary_diagonal_3
 
                     return first_sum - second_sum
+
                 else:
                     # Laplace's Theorem :
 
-                    line = max(matrix, key=lambda x: x.count(0))
+                    line: list[int] = max(matrix, key=lambda x: x.count(0))  # Getting the Horizontal Line with More Zeros.
+                    indexes: list[tuple[int, int]] = [(matrix.index(line), i) for i in range(len(line))]
 
-                    aux_vertical_line = []
-                    indexes = None
-                    temp_indexes = []
+                    # Auxiliary Variables to Check Vertical Lines on the Matrix to apply Laplace's Theorem :
+                    aux_vertical_line: list[int] = []
+                    temp_indexes: list[tuple[int, int]] = []
 
-                    order = len(matrix)
+                    order: int = len(matrix)
 
                     for idx_1 in range(order):
                         for idx_2 in range(order):
+
+                            # Traversing the Vertical Lines of the Matrix :
                             aux_vertical_line.append(matrix[idx_2][idx_1])
                             temp_indexes.append((idx_2, idx_1))
-                        result = max(aux_vertical_line.copy(), line, key=lambda x: x.count(0))
+
+                        result: list[int] = max(aux_vertical_line.copy(), line, key=lambda x: x.count(0))
+
                         if result != line:
                             indexes = temp_indexes.copy()
                             line = result
                         aux_vertical_line.clear()
                         temp_indexes.clear()
 
-                    if indexes:
-                        final_results = []
+                    final_results: list[int] = []  # Variable to Store the Result of Minor Determinants.
 
-                        for index in indexes:
-                            new_temp_matrix = []
-                            for idx_1 in range(order):
-                                temp_line = []
-                                for idx_2 in range(order):
-                                    if idx_1 != index[0] and idx_2 != index[1]:
-                                        temp_line.append(matrix[idx_1][idx_2])
-                                if temp_line:
-                                    new_temp_matrix.append(temp_line)
+                    for index in indexes:
+                        new_temp_matrix: list[list[int]] = []
 
-                            if matrix[index[0]][index[1]]:
-                                final_results.append((-1) ** (sum(index)) * matrix[index[0]][index[1]] * determinant(new_temp_matrix))
-                        return sum(final_results)
+                        for idx_1 in range(order):
+                            temp_line: list[int] = []
+
+                            for idx_2 in range(order):
+
+                                if idx_1 != index[0] and idx_2 != index[1]:
+                                    temp_line.append(matrix[idx_1][idx_2])
+
+                            if temp_line:  # If the temp_line is not empty.
+                                new_temp_matrix.append(temp_line)
+
+                        if matrix[index[0]][index[1]]:
+                            final_results.append((-1) ** (sum(index)) * matrix[index[0]][index[1]] * determinant(new_temp_matrix))
+
+                    return sum(final_results)
+
             else:
-                print("Cannot evaluate a determinant of a matrix", len(matrix), "x", len(matrix[0]))
+                if __name__ == '__main__':
+                    print("Cannot evaluate a determinant of a matrix", len(matrix), "x", len(matrix[0]))
+
         else:
+
             if len(matrix[0]) == 1:
-                return matrix[0]
+                return matrix[0][0]
             else:
                 order = len(matrix[0])
 
                 for row in matrix:
-                    print(row, order)
-                    if len(row) != order:
-                        print("This is not an Matrix.")
-                        return None
 
-                print("Cannot evaluate a determinant of a matrix", len(matrix), "x", len(matrix[0]))
-                return None
+                    if len(row) != order:
+                        if __name__ == '__main__':
+                            print("This is not an Matrix.")
+                        return None
+                
+                if __name__ == '__main__':
+                    print("Cannot evaluate a determinant of a matrix", len(matrix), "x", len(matrix[0]))
