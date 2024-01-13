2740920ffeb34d5667a61cb2c4881542922b6d90
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-01-13 02:30:40 -0300

    Update Prime_Number_Tools.py

--- Prime_Number_Tools.py
+++ Prime_Number_Tools.py
@@ -10,6 +10,9 @@ def check_primality(number: int, /) -> bool:
     elif number == 2:
         return True
 
+    if number % 2 == 0:
+        return False
+
     for i in range(2, int(number ** (1 / 2)) + 1):
         if number % i == 0:
             return False
