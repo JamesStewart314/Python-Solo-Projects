36a45c0d86add48e3ddd9d8034a30eea8351367f
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-02-01 17:55:03 -0300

    Update Files_Sorter-(GUI).py

--- File_Sorter/Files_Sorter-(GUI).py
+++ File_Sorter/Files_Sorter-(GUI).py
@@ -1,6 +1,8 @@
-# / ----------------------------------------------------------------------------------------- \ #
-#   This code was created using the Python language in version 3.12 and higher - 01/30/2024
-# \ ----------------------------------------------------------------------------------------- / #
+# /-----------------------------------------------------------------------------------------------------------------------------------\
+#  This code is a File Sorter created in Python language - version 3.12 or higher - with dependencies on the "customtkinter" library.
+#                  To run it properly, make sure you have these frameworks in your virtual environment.
+#                                                Code Created in ~ 01/31/2024 ~
+# \-----------------------------------------------------------------------------------------------------------------------------------/
 
 
 import os
