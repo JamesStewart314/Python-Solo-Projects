ee5b367fefed5c10fc4a4f6c2b0612404f8b2472
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-02-11 04:53:01 -0300

    Update main.py

--- Website_Checker/main.py
+++ Website_Checker/main.py
@@ -1,8 +1,8 @@
-# /------------------------------------------------------------------------------------------------------------------------------------------------------------\
-#  This code is a File Sorter created in Python language - version 3.12 or higher - with dependencies on the "requests", "emoji" and "fake_useragent" libraries.
-#                                   To run it properly, make sure you have this package in your virtual environment.
-#                                                           Code Created in ~ 01/23/2024 ~
-# /------------------------------------------------------------------------------------------------------------------------------------------------------------/
+# /-----------------------------------------------------------------------------------------------------------------------------------------------------------------\
+#  This code is a Website Checker created in Python language - version 3.12 or higher - with dependencies on the "requests", "emoji" and "fake_useragent" libraries.
+#                                       To run it properly, make sure you have this package in your virtual environment.
+#                                                               Code Created in ~ 01/23/2024 ~
+# /-----------------------------------------------------------------------------------------------------------------------------------------------------------------/
 
 import csv
 import http
