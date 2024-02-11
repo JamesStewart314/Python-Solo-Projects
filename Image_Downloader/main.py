47ad860c41de22b8810715d1989bf79a859e0f4a
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-02-11 04:51:48 -0300

    Update main.py

--- Image_Downloader/main.py
+++ Image_Downloader/main.py
@@ -1,6 +1,8 @@
-# / -------------------------------------------------------------------------------------------------- \ #
-#   This code was created using the Python language and works in versions 3.12 and higher - 01/29/2024
-# \ -------------------------------------------------------------------------------------------------- / #
+# /-----------------------------------------------------------------------------------------------------------------------------\
+#  This code is a File Sorter created in Python language - version 3.12 or higher - with dependencies on the "requests" library.
+#                  To run it properly, make sure you have this package in your virtual environment.
+#                                                Code Created in ~ 01/29/2024 ~
+# \-----------------------------------------------------------------------------------------------------------------------------/
 
 import os
 import itertools
