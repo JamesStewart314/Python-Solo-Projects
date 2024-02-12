71fb910e21754391526800936233bc063b4042f3
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-02-11 21:46:54 -0300

    Add files via upload

---
+++ My_Custom_Modules/GCD_and_LCM Calculator.py
@@ -0,0 +1,130 @@
+# /----------------------------------------------------------------------------------------------------------------------\
+#         This code is a custom module created in Python language - version 3.11 or higher - by me containing a set 
+#  of tools for calculate the least common multiple and greatest common divisor between an arbitrary quantity of numbers.
+#                                              Code Created in ~ 12/25/2023 ~
+# \----------------------------------------------------------------------------------------------------------------------/
+
+import functools
+
+from typing import Iterable
+
+
+def _GCD_2_numbers(number_1: int, number_2: int, /) -> int:
+
+    """
+    
+    Calculates the greatest common divisor between two numbers.
+    (e.g: GCD(2, 3) => 1 ; GCD(7, 49) => 7 ; etc)
+
+    :param number_1: A positive integer.
+    :param number_2: A positive integer.
+    
+    :return: An integer corresponding to the greatest common divisor of
+    the two given numbers.
+    
+    """
+
+    # Euclid's Algorithm :
+    aux_number_1 = max(number_1, number_2)
+    aux_number_2 = min(number_1, number_2)
+
+    remainder = aux_number_1 % aux_number_2
+
+    while remainder:  # Remainder != 0
+        aux_number_1 = aux_number_2
+        aux_number_2 = remainder
+        remainder = aux_number_1 % aux_number_2
+
+    return aux_number_2
+
+
+def _LCM_2_numbers(number_1: int, number_2: int, /) -> int:
+
+    """
+    
+    Calculates the least common multiple between two numbers.
+    (e.g: LCM(2, 3) => 6 ; LCM(7, 49) => 49 ; etc)
+
+    :param number_1: A positive integer.
+    :param number_2: A positive integer.
+    
+    :return: An integer corresponding to the least common multiple of
+    the two given numbers.
+    
+    """
+
+    # The product of the GCD and LCM of two natural numbers
+    # is equal to the product of those numbers, so
+    # LCM(n1, n2) = n1 * n2 / GCD(n1, n2) :
+    
+    return number_1 * number_2 // _GCD_2_numbers(number_1, number_2)
+
+
+def GCD(number_iterator: tuple[int, ...] | list[int], /) -> int:
+
+    """
+    
+    Calculates the greatest common divisor between two or more numbers present in an Iterable.
+    (e.g: GCD((2, 3, 5)) => 1 ; GCD([7, 49, 21]) => 7 ; etc)
+
+    :param number_iterator: An Iterable containing two or more positive integers.
+    
+    :return: An integer corresponding to the greatest common divisor of
+    all numbers present in the Iterable.
+    
+    """
+    
+    if isinstance(number_iterator, Iterable) and len(number_iterator) >= 2:
+
+        ######################################################################################
+        #                     Command Block for Preventive Verification :                    #
+        #             (removable if necessary to improve algorithm performance)              #
+        
+        for element in number_iterator:
+            if isinstance(element, int):
+                if element <= 0:
+                    raise ValueError("Iterable must only contain non-zero positive integers.")
+            else:
+                raise TypeError("Iterable must only contain non-zero positive integers.")
+        #                                                                                    #
+        ######################################################################################
+
+        return functools.reduce(_GCD_2_numbers, number_iterator)
+
+    else:
+        raise TypeError("To calculate GCD, we need to have at least two numbers in an iterator.")
+
+
+def LCM(number_iterator: tuple[int, ...] | list[int], /) -> int:
+
+    """
+    
+    Calculates the least common multiple between two or more numbers present in an Iterable.
+    (e.g: LCM((2, 3, 5)) => 30 ; LCM([7, 49, 21]) => 147 ; etc)
+
+    :param number_iterator: An Iterable containing two or more positive integers.
+    
+    :return: An integer corresponding to the least common multiple of
+    all numbers present in the Iterable.
+    
+    """
+    
+    if isinstance(number_iterator, Iterable) and len(number_iterator) >= 2:
+        
+        ######################################################################################
+        #                     Command Block for Preventive Verification :                    #
+        #             (removable if necessary to improve algorithm performance)              #
+        
+        for element in number_iterator:
+            if isinstance(element, int):
+                if element <= 0:
+                    raise ValueError("Iterable must only contain non-zero positive integers.")
+            else:
+                raise TypeError("Iterable must only contain non-zero positive integers.")
+        #                                                                                    #
+        ######################################################################################
+
+        return functools.reduce(_LCM_2_numbers, number_iterator)
+
+    else:
+        raise TypeError("To calculate LCM, we need to have at least two numbers in an iterator.")
