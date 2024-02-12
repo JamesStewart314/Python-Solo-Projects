eeaf6897cbb7dc7be9c6da98ab20f2894d19d59f
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-02-11 22:54:42 -0300

    Update Prime_Number_Tools.py

--- My_Custom_Modules/Prime_Number_Tools.py
+++ My_Custom_Modules/Prime_Number_Tools.py
@@ -17,7 +17,7 @@ def check_primality(number: int, /) -> bool:
      The function checks if the given number is prime or not.
     
     :param number: A positive integer number.
-    :return: Returns a boolean ("True" if the number is prime, "False" if it's not.)
+    :return: Returns a boolean ("True" if the number is prime, "False" if it's not)
 
     """
     
