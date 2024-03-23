4a88984ff358e33557185331c198615a2b98d4ac
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-03-23 12:22:50 -0300

    Update models.py

--- Weather_APP/models.py
+++ Weather_APP/models.py
@@ -4,7 +4,7 @@ from datetime import datetime as dt
 import colorama as clr
 
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, override
 
 
 @dataclass(order=False, frozen=True)
@@ -74,10 +74,12 @@ class ThreadWithReturnValue(Thread):
         Thread.__init__(self, group, target, name, args, kwargs)
         self._return = None
     
+    @override
     def run(self) -> None:
         if self._target is not None:
             self._return = self._target(*self._args, **self._kwargs)
     
+    @override
     def join(self, *args) -> Any:
         Thread.join(self, *args)
         return self._return
